193be7318e94a4ad190299516b8aaa396016fc14
Ryan <141914774+rndmzd@users.noreply.github.com>
2024-12-27 21:44:15 -0600

    Proxy curl requests through nordlynx.

--- api.php
+++ api.php
@@ -3,16 +3,17 @@
 
   function get_data($url) {
     $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt( $ch, CURLOPT_ENCODING, "" );
-    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-    curl_setopt( $ch, CURLOPT_AUTOREFERER, true );
-    curl_setopt( $ch, CURLOPT_MAXREDIRS, 10 );
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
-    curl_setopt($ch,CURLOPT_MAXCONNECTS,320);
-    curl_setopt($ch,CURLOPT_TIMEOUT,30);
-    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,30);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_ENCODING, "");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_MAXCONNECTS, 320);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($ch, CURLOPT_INTERFACE, "nordlynx");
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
